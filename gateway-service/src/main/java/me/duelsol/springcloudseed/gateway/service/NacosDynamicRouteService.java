e7ba9bd82b6e8409b6e3493ae8465f42bff962c9
Duelsol <duelsol@sina.com>
2022-01-21 17:35:07 +0800

    Feign使用Sentinel；完成动态路由。

--- gateway-service/src/main/java/me/duelsol/springcloudseed/gateway/service/NacosDynamicRouteService.java
+++ gateway-service/src/main/java/me/duelsol/springcloudseed/gateway/service/NacosDynamicRouteService.java
@@ -1,10 +1,14 @@
 package me.duelsol.springcloudseed.gateway.service;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.nacos.api.config.annotation.NacosConfigListener;
-import com.google.common.collect.Lists;
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.shaded.com.google.gson.Gson;
+import com.google.common.reflect.TypeToken;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
@@ -13,7 +17,10 @@ import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 /**
  * @author 冯奕骅
@@ -22,70 +29,70 @@ import java.util.List;
 @Slf4j
 public class NacosDynamicRouteService implements ApplicationEventPublisherAware {
 
+    private static final String DATAID = "gateway";
+
+    private static final String GROUP = "DEFAULT_GROUP";
+
+    @Value("${spring.cloud.nacos.discovery.server-addr}")
+    private String serverAddr;
+
     @Autowired
     private RouteDefinitionWriter routeDefinitionWriter;
 
     private ApplicationEventPublisher applicationEventPublisher;
 
-    /**
-     * 路由id
-     */
-    private static final List<String> ROUTE_IDS = Lists.newArrayList();
+    private static final List<String> ROUTE_LIST = new ArrayList<>();
 
-    /**
-     * 监听Nacos路由配置，动态改变路由
-     *
-     * @param configInfo
-     */
-    @NacosConfigListener(dataId = "gateway-service")
-    public void routeConfigListener(String configInfo) {
-        clearRoute();
+    @PostConstruct
+    public void dynamicRouteConfigListener() {
         try {
-            List<RouteDefinition> gatewayRouteDefinitions = JSON.parseArray(configInfo, RouteDefinition.class);
-            for (RouteDefinition routeDefinition : gatewayRouteDefinitions) {
-                addRoute(routeDefinition);
-            }
-            publish();
-            log.info("Dynamic Routing Publish Success");
-        } catch (Exception e) {
+            ConfigService configService = NacosFactory.createConfigService(serverAddr);
+            String configInfo = configService.getConfig(DATAID, GROUP, 5000);
+            parseRoutes(configInfo);
+            configService.addListener(DATAID, GROUP, new Listener() {
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    parseRoutes(configInfo);
+                }
+
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+            });
+        } catch (NacosException e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    /**
-     * 清空路由
-     */
-    private void clearRoute() {
-        for (String id : ROUTE_IDS) {
-            routeDefinitionWriter.delete(Mono.just(id)).subscribe();
+    private void parseRoutes(String configInfo) {
+        clearRoutes();
+        List<RouteDefinition> gatewayRouteDefinitions = new Gson().fromJson(configInfo, new TypeToken<List<RouteDefinition>>(){}.getType());
+        for (RouteDefinition routeDefinition : gatewayRouteDefinitions) {
+            addRoute(routeDefinition);
         }
-        ROUTE_IDS.clear();
+        publish();
+    }
+
+    private void clearRoutes() {
+        for (String id : ROUTE_LIST) {
+            this.routeDefinitionWriter.delete(Mono.just(id)).subscribe();
+        }
+        ROUTE_LIST.clear();
+    }
+
+    private void addRoute(RouteDefinition definition) {
+        routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+        ROUTE_LIST.add(definition.getId());
+    }
+
+    private void publish() {
+        this.applicationEventPublisher.publishEvent(new RefreshRoutesEvent(this.routeDefinitionWriter));
     }
 
     @Override
     public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
         this.applicationEventPublisher = applicationEventPublisher;
-    }
-
-    /**
-     * 添加路由
-     *
-     * @param definition
-     */
-    private void addRoute(RouteDefinition definition) {
-        try {
-            routeDefinitionWriter.save(Mono.just(definition)).subscribe();
-            ROUTE_IDS.add(definition.getId());
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-
-    /**
-     * 发布路由、使路由生效
-     */
-    private void publish() {
-        this.applicationEventPublisher.publishEvent(new RefreshRoutesEvent(this.routeDefinitionWriter));
     }
 
 }
