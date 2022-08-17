a43525148438051ff18afbaab17b208841cf0bd8
Duelsol <duelsol@sina.com>
2022-08-17 16:37:04 +0800

    测试路径匹配判断。

--- gateway-service/src/main/java/me/duelsol/springcloudseed/gateway/AuthGlobalFilter.java
+++ gateway-service/src/main/java/me/duelsol/springcloudseed/gateway/AuthGlobalFilter.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.result.method.RequestMappingInfo;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -15,6 +16,11 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 判断当前请求路径是否匹配
+        RequestMappingInfo mapping = RequestMappingInfo.paths("/user/*").build().getMatchingCondition(exchange);
+        if (mapping != null) {
+            System.out.println(mapping);
+        }
         return chain.filter(exchange);
     }
 
